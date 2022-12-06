cd02e2cd37767cee3cb645d3045ada821f5a9553
Valeria Martynova <kthf175@gmail.com>
2022-12-06 21:42:30 +0500

    Added task13

---
+++ practice2/task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if  (num >= 100)
+    {
+        char thirdNumber = num.ToString()[2];
+        Console.WriteLine(thirdNumber);
+    }
+else 
+    Console.WriteLine("третьей цифры нет");
