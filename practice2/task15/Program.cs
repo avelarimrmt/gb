14ca817d68fe6516688bccfe3ed6d5cced582d3b
Valeria Martynova <kthf175@gmail.com>
2022-12-06 21:51:51 +0500

    Added tack15

---
+++ practice2/task15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number){
+    case 1:
+        Console.WriteLine("нет");
+        break;
+    case 2:
+        Console.WriteLine("нет");
+        break;
+    case 3:
+        Console.WriteLine("нет");
+        break;
+    case 4:
+        Console.WriteLine("нет");
+        break;
+    case 5:
+        Console.WriteLine("нет");
+        break;
+    case 6:
+        Console.WriteLine("да");
+        break;
+    case 7:
+        Console.WriteLine("да");
+        break;
+    default:
+         Console.WriteLine("Таких значений нет");
+        break;
+}
