ff97cdf6f8a8b5c0fe67d9b06343f7892a425bce
Valeria Martynova <kthf175@gmail.com>
2022-12-06 20:02:58 +0500

    Added task8

---
+++ practice1/task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=N; i++)
+{
+    if (i%2 == 0)
+    {
+        Console.Write(i);
+        if (i != N && i != N-1) 
+            Console.Write(",");
+    }
+}
