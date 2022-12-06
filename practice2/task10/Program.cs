f3ab0a9383246888534a1a0a47e3d2a65bb29813
Valeria Martynova <kthf175@gmail.com>
2022-12-06 20:46:53 +0500

    Added task10

---
+++ practice2/task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number/100 != 0 && number/1000 == 0)
+    Console.WriteLine(number%100/10);
