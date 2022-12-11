d6672e322c27f3dd548277ab5c0db299155194d4
Valeria Martynova <kthf175@gmail.com>
2022-12-11 15:57:08 +0500

    Added task23

---
+++ practice3/task23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+        Console.Write($"{Math.Pow(i, 3)}");
+        if (i != n)
+            Console.Write(", ");
+}
