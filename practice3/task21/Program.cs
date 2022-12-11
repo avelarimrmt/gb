2640f116c43cd55417b9ed6212f3ce4146788e29
Valeria Martynova <kthf175@gmail.com>
2022-12-11 15:43:18 +0500

    Added task21

---
+++ practice3/task21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A ({x1}, {y1}, {z1})");
+
+Console.WriteLine("Введите координаты второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"B ({x2}, {y2}, {z2})");
+
+double result = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
+Console.WriteLine(Math.Round(result, 2));
