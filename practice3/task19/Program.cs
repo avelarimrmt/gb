07336574796240f306a582921781811f2589fdfd
Valeria Martynova <kthf175@gmail.com>
2022-12-11 14:50:49 +0500

    Added task19

---
+++ practice3/task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = number.ToString();
+int size = str.Length;
+bool isPalindrom = true;
+
+if (size == 5)
+{
+    for (int i = 0; i < size/2; i++)
+    {
+        if (str[i] != str[size - i - 1]) 
+            isPalindrom = false;
+    }
+
+    if (isPalindrom) 
+        Console.WriteLine("да");
+    else 
+        Console.WriteLine("нет");
+}
+else 
+    Console.WriteLine("Введено не пятизначное число");
