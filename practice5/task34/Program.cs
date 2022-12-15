ac09e607fe6bc2500fe65ab271cccafd20700821
Valeria Martynova <kthf175@gmail.com>
2022-12-15 20:14:53 +0500

    Added task34

---
+++ practice5/task34/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = randArray();
+Console.WriteLine(string.Join(", ", arr));
+Console.WriteLine(CountEvenNumbers(arr));
+
+int[] randArray()
+{   
+    int[] array = new int[4];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0,300);
+    }
+
+    return array;
+}
+
+int CountEvenNumbers(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) 
+            count++;
+    }
+
+    return count;
+}
