ae2e5ff91bb2faaca02ad782aca935404577f3c0
Valeria Martynova <kthf175@gmail.com>
2022-12-15 19:17:25 +0500

    Added task27

---
+++ practice4/task27/Program.cs
@@ -0,0 +1,35 @@
+﻿int number = InputNumber();
+Console.WriteLine(Sum(number));
+
+int InputNumber()
+{
+    string str;
+    int number;
+    
+    while (true)
+    {
+        Console.WriteLine("Введите число");
+        str = Console.ReadLine();
+        if (int.TryParse(str, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз");
+    }
+
+    return number;
+}
+
+int Sum(int num)
+{
+    int result = 0;
+    string str = num.ToString(); 
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+
+    return result;
+}
