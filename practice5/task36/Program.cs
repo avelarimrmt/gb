ad6d31586f2da48038b1b0234d38cd9307b57334
Valeria Martynova <kthf175@gmail.com>
2022-12-15 20:23:16 +0500

    Added task36

---
+++ practice5/task36/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = randArray();
+Console.WriteLine(string.Join(", ", arr));
+Console.WriteLine(SumOnOdd(arr));
+
+int[] randArray()
+{   
+    int[] array = new int[4];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(-10, 10);
+    }
+
+    return array;
+}
+
+int SumOnOdd(int[] arr)
+{
+    int result = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) 
+            result = result + arr[i];
+    }
+
+    return result;
+}
