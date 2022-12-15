29c046e4f76de4bd5edf94dbeead2d820242e02f
Valeria Martynova <kthf175@gmail.com>
2022-12-15 19:59:28 +0500

    Added task29

---
+++ practice4/task29/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = randArray();
+Console.WriteLine(string.Join(", ", arr));
+
+int[] randArray()
+{   
+    int[] array = new int[8];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0,99);
+    }
+
+    return array;
+}
