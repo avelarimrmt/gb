1bb3b244bfd8619019d4ebf86ca0fec85eb34f94
Valeria Martynova <kthf175@gmail.com>
2023-01-15 17:20:30 +0500

    Added task66

---
+++ practice9/task66/Program.cs
@@ -0,0 +1,35 @@
+﻿int m = InputNumber("значение M");
+int n = InputNumber("значение N");
+PrintNumber(SumAllNaturalNumbers(m, n));
+
+int InputNumber(string text)
+{
+    int number;
+    
+    while (true)
+    {
+        Console.WriteLine($"Введите {text}");
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз");
+
+    }
+
+    return number;
+}
+
+int SumAllNaturalNumbers(int a, int b)
+{
+    if (a == b)
+        return b;
+    
+    return b + SumAllNaturalNumbers(a, b-1);
+}
+
+void PrintNumber(int num)
+{   
+    Console.Write("Сумма натуральных элементов в промежутке от M до N: ");
+    Console.Write(num);
+}
