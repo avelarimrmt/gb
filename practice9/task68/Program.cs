be51592e855877132a45506a3b3bab60ea2532de
Valeria Martynova <kthf175@gmail.com>
2023-01-15 17:29:40 +0500

    Added task68

---
+++ practice9/task68/Program.cs
@@ -0,0 +1,32 @@
+﻿int m = InputNumber("значение M");
+int n = InputNumber("значение N");
+
+Console.WriteLine($"A({m},{n}) = {AckermanFunction(m, n)}");
+
+int InputNumber(string text)
+{
+    int number;
+    
+    while (true)
+    {
+        Console.WriteLine($"Введите {text}");
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз");
+
+    }
+
+    return number;
+}
+
+int AckermanFunction(int a, int b)
+{
+    if (a == 0)
+        return b + 1;
+    if (a > 0 && b == 0)
+        return AckermanFunction(a - 1, 1);
+    else
+        return AckermanFunction(a - 1, AckermanFunction(a, b - 1));
+}
