04d23388ffeed3dac153af1e8edc938bbeba46ea
Valeria Martynova <kthf175@gmail.com>
2023-01-15 16:42:41 +0500

    Added task64

---
+++ practice9/task64/Program.cs
@@ -0,0 +1,42 @@
+﻿int n = InputNumber("значение N");
+int m = 1;
+AllNaturalNumbers(n, m);
+
+int InputNumber(string text)
+{
+    int number;
+    
+    while (true)
+    {
+        Console.WriteLine($"Введите {text}");
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз");
+    }
+
+    return number;
+}
+
+int AllNaturalNumbers(int a, int b)
+{
+    if (a == b)
+    {
+        PrintNumber(b);
+        return b;
+    }
+    else 
+        AllNaturalNumbers(a, b+1);
+
+    PrintNumber(b);
+    return b;
+}
+
+void PrintNumber(int num)
+{
+    Console.Write(num);
+
+    if (num != m) 
+        Console.Write(", ");
+}
